356e7d745ac600d1d8aa2379c7ffef171a1dac1c
ocean <osciris.veliz@gmail.com>
2022-07-25 21:22:43 -0500

    create Employee test and mock

--- __tests__/Employee.test.js
+++ __tests__/Employee.test.js
@@ -1,8 +1,12 @@
+const Employee = require('../lib/Employee');
+
+jest.mock('../lib/Employee');
+
 test('creates an employee object', () => {
   const employee = new Employee('Bob');
 
   expect(this.name).toBe('Bob');
-  expect(this.id).toBe(expect.any(Number));
+  expect(this.id).toEqual(expect.any(Number));
   expect(this.email).toBe(expect.stringContaining(string));
-  expect(this.role).toBe
+  expect(this.role).toBe(expect.stringContaining(string));
 });
