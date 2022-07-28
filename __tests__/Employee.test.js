7f09fce8dd9d3fccfced8b53402458e4c3396b80
ocean <osciris.veliz@gmail.com>
2022-07-28 18:06:00 -0500

    separate inquirer prompts to index.js and finish Employee methods

--- __tests__/Employee.test.js
+++ __tests__/Employee.test.js
@@ -1,17 +1,41 @@
 const Employee = require('../lib/Employee');
 
-jest.mock('../lib/Employee')
+jest.mock('../lib/Employee');
 
 test('creates an employee object', () => {
   const employee = new Employee('Bob');
+  expect(typeof(employee)).toBe('object');
 
   expect(employee.name).toBe('Bob');
   expect(employee.id).toEqual(expect.any(Number));
   expect.stringContaining(employee.email);
 });
 
-test('gets employee name via prompt')
+test('returns employees name', () => {
+  const employee = new Employee('Bob');
 
-// test getId()
+  expect(employee.getName).toBe('Bob');
+});
+
+
+// test for getId()
+// test('returns employees ID', () => {
+//   const newId = 1234;
+//   const employee = new Employee('Bob', newId);
+
+//   expect(employee.getId).toBe(newId);
+// });
+
+// test('returns employees ID', () => {
+//   return getId().then(data => {
+//     expect(data).toBe('')
+//   })
+//   const newId = 1234;
+//   const employee = new Employee('Bob', newId);
+
+//   expect(employee.getId).toBe(newId);
+// });
+
+
 // test getEmail()
 // test getRole() -- returns 'Employee'
--- index.js
+++ index.js
@@ -1,7 +1,45 @@
 const Employee = require('./lib/Employee');
+// import other classes
+const inquirer = require('inquirer');
+
 
 function startQuestions() {
+  // start with questions for Manager
+  inquirer.prompt([
+    {
+    type: 'text',
+    name: 'name',
+    message: "What is the team manager's name?"
+    },
+    {
+      type: 'text',
+      name: 'id',
+      message: 'What is their employee ID?'
+    },
+    {
+      type: 'text',
+      name: 'email',
+      message: 'What is your email?'
+    }
+  ]
+  ).then(data => {
+    const newManager = new Manager(data.name, data.id, data.email)
+  })
+
+
+  
 
 }
 
 startQuestions();
+
+
+   // .then(({ name }) => {
+  //   this.name = new Employee name;
+  //   console.log(this.name);
+  // })
+
+  // inquirer.prompt()
+  // .then(({ id }) => {
+  //   this.id = id;
+  // })
