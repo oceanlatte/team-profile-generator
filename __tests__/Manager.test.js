232d57e1528f45d4a34f3d1c78571c3a4bd2d8b1
ocean <osciris.veliz@gmail.com>
2022-07-29 21:55:40 -0500

    add Manager methods and tests for new methods

--- __tests__/Manager.test.js
+++ __tests__/Manager.test.js
@@ -7,5 +7,13 @@ test('creates manager object', () => {
 });
 
 // test for office number
+test('method returns office number', () => {
+  const manager = new Manager('Sarah', 123, 'email', 456);
+  expect(manager.officeNumber()).toBe(456);
+})
 
 // test for role -- returns: Manager
+test('method returns manager role name', () => {
+  const manager = new Manager('Sarah');
+  expect(manager.getRole()).toBe('Manager');
+})
